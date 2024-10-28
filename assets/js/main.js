abeade3ff2bba6a222012fc316f574cc0eb9d32e
Ethicdev07 <akinadejunior76@gmail.com>
2024-10-28 09:00:21 -0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,7 +49,7 @@ const sendEmail = (event)=>{
             contactMessage.textContent = ''
         },5000)
 
-        contactForm.reset()
+        contactForm.reset();
     })
     .catch((error)=>{
         contactMessage.textContent = 'Message not sent ❌, please try again'
@@ -92,16 +92,16 @@ window.addEventListener('scroll', scrollActive)
     
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
-// const sr = scrollReveal({
-//     origin: 'top',
-//     distance: '60px',
-//     duration: 2500,
-//     delay: 400,
-//     reset: true // animation will repeat
-// })
-// sr.reveal(`.home__data, .experience, .skills, .contact__container`)
-// sr.reveal(`.home__img`, {delay:600})
-// sr.reveal(`.home__scroll`, {delay:800})
-// sr.reveal(`.work__card, .services__card`, {interval: 100})
-// sr.reveal(`.about__content`, {origin: 'right'})
-// sr.reveal(`.about__img`, {origin: 'left'})
+const sr = scrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true // animation will repeat
+})
+sr.reveal(`.home__data, .experience, .skills, .contact__container`)
+sr.reveal(`.home__img`, {delay:600})
+sr.reveal(`.home__scroll`, {delay:800})
+sr.reveal(`.work__card, .services__card`, {interval: 100})
+sr.reveal(`.about__content`, {origin: 'right'})
+sr.reveal(`.about__img`, {origin: 'left'})
